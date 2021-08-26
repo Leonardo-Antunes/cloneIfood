c4e74f02e0eb36d10e9a9808aeec4737edb8b87e
Leonardo-Antunes <leonardo.antunes@globalhitss.com.br>
2021-08-26 15:34:00 -0300

    finalized app

---
+++ src/pages/Perfil/index.js
@@ -0,0 +1,15 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaView, Image, View, ButtonContainer } from "./style";
+import { Text } from "react-native";
+
+export default function Perfil() {
+  return (
+    <>
+      <StatusBar style="theme-dark" />
+      <SafeAreaView>
+        <Text>Principal</Text>
+      </SafeAreaView>
+    </>
+  );
+}
